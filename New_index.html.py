9136198130b2456f1d3dd96ef24d88f533fba25a
Edwin Saul <edwinsaulpm@gmail.com>
2022-01-25 16:20:12 -0500

    first scripts

---
+++ New_index.html.py
@@ -0,0 +1,15 @@
+#make a new index.html file
+
+texto="""<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>  </title>
+  </head>
+  <body>
+
+  </body>
+</html>"""
+f=open("index.html","w")
+f.write(texto)
+f.close()
