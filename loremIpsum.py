6659633cbe026c7b78fb640a6c6dc9b00b6350f5
Edwin Saul <edwinsaulpm@gmail.com>
2022-02-27 23:35:15 -0500

    new svg.py

--- loremIpsum.py
+++ loremIpsum.py
@@ -1,4 +1,6 @@
 #puts Lorem ipsum text on the OS clipboard
+from sys import version_info
+print(version_info)
 import pyperclip
 texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum "
 pyperclip.copy(texto)
